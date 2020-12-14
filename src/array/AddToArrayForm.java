04827b6d33e358b9314ba2c037b61890c29ecf6b
wenjianli3 <wenjianli3@jd.com>
2020-12-14 11:24:43 +0800

    数组-数组形式的整数加法优化

--- src/array/AddToArrayForm.java
+++ src/array/AddToArrayForm.java
@@ -1,6 +1,8 @@
 package array;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -23,10 +25,11 @@ import java.util.List;
  */
 public class AddToArrayForm {
     public static void main(String[] args) {
-        int []A = new int[]{1,2,0,0};
-        int k = 34;
-        System.out.println(addToArrayFrom(A,k));
+        int []A = new int[]{9,9,9,9,9,9,9,9,9,9};
+        int k = 1;
+        System.out.println(addToArrayFromBetter(A,k));
     }
+    // 自己写 报错溢出
     public static List<Integer> addToArrayFrom(int []A, int k){
         int sum = 0;
         List<Integer> list = new ArrayList<>();
@@ -41,6 +44,26 @@ public class AddToArrayForm {
             list.add(num);
         }
         return list;
+    }
+
+    // 逐位相加： 123+912123+912123+912，我们把它表示成 [1,2,3+912][1, 2, 3+912][1,2,3+912]。然后，我们计算 3+912=9153+912 = 9153+912=915。555 留在当前这一位，将 910/10=91910/10=91910/10=91 以进位的形式加入下一位。
+    //然后，我们再重复这个过程，计算 [1,2+91,5][1, 2+91, 5][1,2+91,5]。我们得到 939393，333 留在当前位，将 90/10=990/10=990/10=9 以进位的形式加入下一位。继而又得到 [1+9,3,5][1+9, 3, 5][1+9,3,5]，重复这个过程之后，最终得到结果 [1,0,3,5][1, 0, 3, 5][1,0,3,5]。
+
+    public static List<Integer> addToArrayFromBetter(int []A, int K){
+        int N = A.length;
+        int cur = K;
+        List<Integer> ans = new ArrayList();
+
+        int i = N;
+        while (--i >= 0 || cur > 0) {
+            if (i >= 0)
+                cur += A[i];
+            ans.add(cur % 10);
+            cur /= 10;
+        }
+
+        Collections.reverse(ans);
+        return ans;
     }
 
 }
