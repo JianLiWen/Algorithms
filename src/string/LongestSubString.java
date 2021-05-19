65565df28c6e8ab50e2dd9af20e64ab7e454b636
wenjianli3 <wenjianli3@jd.com>
2021-05-19 20:13:01 +0800

    字符串-无重复字符的最长字串优化（滑动窗口）

--- src/string/LongestSubString.java
+++ src/string/LongestSubString.java
@@ -25,10 +25,10 @@ public class LongestSubString {
 
     public static void main(String[] args) {
         String s = "abcabcbb";
-        System.out.println(lengthOfLongestSubstring(s));
+        System.out.println(lengthOfLongestSubstringBetter(s));
     }
 
-    // mine-通过，但是太耗时，74ms
+    // mine-通过，但是太耗时，74ms 初步思想是：如果集合中存在，并且判断是不是紧挨的，如果是的话就清掉重新put，如果不是，则把已存在对应index之后的字符加入map，主要耗时就在while循环时重新加入
     private static int lengthOfLongestSubstring(String s){
         if (s.length()==1){
             return 1;
@@ -54,6 +54,24 @@ public class LongestSubString {
             if (i==s.length()-1){
                 maxLength = maxLength >map.size() ? maxLength : map.size();
             }
+        }
+        return maxLength;
+    }
+
+    // 优化方法：采用滑动窗口 优化思想为：设定两个指针，左指针进行移动，主要是找到最大的那个位置
+    private static int lengthOfLongestSubstringBetter(String s){
+        if (s.length()==0){
+            return 0;
+        }
+        Map<Character,Integer> map = new HashMap<>();
+        int maxLength =0 ;
+        int left = 0;
+        for (int i=0;i<=s.length()-1;i++){
+            if (map.containsKey(s.charAt(i))){
+                left = Math.max(left,map.get(s.charAt(i))+1);
+            }
+            map.put(s.charAt(i),i);
+            maxLength = Math.max(maxLength,i-left+1);
         }
         return maxLength;
     }
