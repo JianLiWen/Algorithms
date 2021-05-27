e565fbdcb74c2210de30eaddec0881197997f3af
wenjianli3 <wenjianli3@jd.com>
2021-05-27 16:39:19 +0800

    数组-整数反转（最优解）

--- src/array/Reverse.java
+++ src/array/Reverse.java
@@ -55,4 +55,19 @@ public class Reverse {
         return num.intValue();
     }
 
+    // 最优解法
+    public int reverseBetter(int x) {
+
+        int rev = 0;
+        while (x != 0) {
+            if (rev < Integer.MIN_VALUE / 10 || rev > Integer.MAX_VALUE / 10) {
+                return 0;
+            }
+            int digit = x % 10;
+            x /= 10;
+            rev = rev * 10 + digit;
+        }
+        return rev;
+
+    }
 }
