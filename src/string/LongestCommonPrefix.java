e5f0473c7203ea1fb71d6adca467b67ab06a62a6
wenjianli3 <wenjianli3@jd.com>
2021-05-28 17:47:16 +0800

    字符串-最长公共前缀(最优解)

--- src/string/LongestCommonPrefix.java
+++ src/string/LongestCommonPrefix.java
@@ -71,4 +71,20 @@ public class LongestCommonPrefix {
         return stringBuffer.substring(0,sub);
     }
 
+    public static String isbetter(String[] strs){
+        if(strs.length == 0)
+            return "";
+        String ans = strs[0];
+        for(int i =1;i<strs.length;i++) {
+            int j=0;
+            for(;j<ans.length() && j < strs[i].length();j++) {
+                if(ans.charAt(j) != strs[i].charAt(j))
+                    break;
+            }
+            ans = ans.substring(0, j);
+            if(ans.equals(""))
+                return ans;
+        }
+        return ans;
+    }
 }
