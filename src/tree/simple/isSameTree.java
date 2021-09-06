1d6975ae2c048cc57600fc9bfb4a5c9239fc1ff9
wenjianli3 <wenjianli3@jd.com>
2021-09-06 20:38:49 +0800

    二叉树-相同的树

--- src/tree/simple/isSameTree.java
+++ src/tree/simple/isSameTree.java
@@ -7,23 +7,23 @@ package tree.simple;
  */
 public class isSameTree {
     public static void main(String[] args) {
-        TreeNode treeNode1 = new TreeNode(3);
-        treeNode1.left = new TreeNode(9);
-        treeNode1.right = new TreeNode(20);
-        treeNode1.right.left = new TreeNode(15);
-        TreeNode treeNode2 = new TreeNode(3);
-        treeNode1.left = new TreeNode(9);
-        treeNode1.right = new TreeNode(20);
+        TreeNode treeNode1 = new TreeNode(1);
+        treeNode1.left = new TreeNode(2);
+        TreeNode treeNode2 = new TreeNode(1);
+        treeNode2.right = new TreeNode(2);
         System.out.println(isSameTree(treeNode1,treeNode2));
     }
 
     public static boolean isSameTree(TreeNode p, TreeNode q) {
-        if (p==null || q==null) return false;
-        if (p.val != q.val)  return  false;
+        if (p == null && q==null) {
+            return true;
+        }else if (p==null || q==null) {
+            return  false;
+        }else if (p.val != q.val) {
+            return  false;
+        }
         Boolean left = isSameTree(p.left,q.left);
         Boolean right = isSameTree(p.right,q.right);
-        if (left && right) return true;
-        if (!left && !right) return  true;
-        return false;
+        return left && right;
     }
 }
